fb53dae23d922bcfa665f0c494e4802b6fe68927
Robert Hux <rhux@ipsiti.com>
2011-03-09 19:08:58 -0500

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,8 +15,7 @@ describe PagesController do
     
     it "should have the right title" do
         get 'home'
-        response.should have_selector("title", 
-            :content => @base_title + " | Home")
+        response.should have_selector("title", :content => @base_title + " | Home")
         end
   end
 
